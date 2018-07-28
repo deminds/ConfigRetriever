d08a0c3b78a74f60fd62eeebc57cfd81b8242fc8
Dmitry Demin <demin.ds@mail.ru[>
2018-07-28 22:24:03 +0300

    Add AttributeExtensions helper

---
+++ GH.DD.ConfigRetriever/Helpers/AttributeExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace GH.DD.ConfigRetriever.Helpers
+{
+    public static class AttributeExtensions
+    {
+
+        public static TValue GetAttributeValue<TAttribute, TValue>(this Type type,
+            Func<TAttribute, TValue> valueSelector,
+            bool inherit)
+            where TAttribute : Attribute
+        {
+            var value = type
+                    .GetCustomAttribute(typeof(TAttribute), inherit)
+                as TAttribute;
+
+            return value == null ? default(TValue) : valueSelector(value);
+        }
+        
+        public static TValue GetAttributeValue<TAttribute, TValue>(this PropertyInfo property,
+            Func<TAttribute, TValue> valueSelector,
+            bool inherit)
+            where TAttribute : Attribute
+        {
+            var value = property
+                    .GetCustomAttribute(typeof(TAttribute), inherit)
+                as TAttribute;
+
+            return value == null ? default(TValue) : valueSelector(value);
+        }
+
+        public static bool HasAttribute<TAttribute>(this Type type)
+            where TAttribute : Attribute
+        {
+            return Attribute.IsDefined(type, typeof(TAttribute));
+        }
+        
+        public static bool HasAttribute<TAttribute>(this PropertyInfo property)
+            where TAttribute : Attribute
+        {
+            return Attribute.IsDefined(property, typeof(TAttribute));
+        }
+    }
+}
