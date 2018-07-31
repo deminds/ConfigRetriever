6f998399b731caea7f1f543cb9f31b89eb5e21d0
Dmitry Demin <demin.ds@mail.ru[>
2018-08-01 01:16:58 +0300

    ConfigElement add check type

--- GH.DD.ConfigRetriever/ConfigElement.cs
+++ GH.DD.ConfigRetriever/ConfigElement.cs
@@ -13,11 +13,17 @@ namespace GH.DD.ConfigRetriever
 
         public ConfigElement(string name, string nameConfigProperty, List<string> path, Type elementType, bool canFloatUp)
         {
+            if (elementType != typeof(string) &&
+                elementType != typeof(int) &&
+                elementType != typeof(long) &&
+                elementType != typeof(double))
+                throw new ArgumentException($"ConfigElement field ElementType must be: string, int, long or double. ElementType is {elementType}");
+            
             Name = name ?? throw new ArgumentNullException(nameof(name));
             NameConfigProperty = nameConfigProperty ?? throw new ArgumentNullException(nameof(nameConfigProperty));
             Path = path ?? throw new ArgumentNullException(nameof(path));
-            ElementType = elementType ?? throw new ArgumentNullException(nameof(elementType));
             CanFloatUp = canFloatUp;
+            ElementType = elementType;
         }
 
         public IEnumerable<ConfigElement> FloatUp()
