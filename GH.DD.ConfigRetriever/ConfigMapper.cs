0abf55365d4c59a413abee023822f51fbfb4faa4
Dmitry Demin <demin.ds@mail.ru[>
2018-08-14 01:50:22 +0300

    Add ConfigMapper
    Add IConfigMapper

---
+++ GH.DD.ConfigRetriever/ConfigMapper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GH.DD.ConfigRetriever.Helpers;
+
+namespace GH.DD.ConfigRetriever
+{
+    public class ConfigMapper<TItem> : IConfigMapper 
+        where TItem : class, new()
+    {
+        private TItem _config;
+        
+        // Dictionary propertyName on ConfigMapper
+        private Dictionary<string, IConfigMapper> _children;
+
+        public ConfigMapper()
+        {
+            _config = new TItem();
+            _children = new Dictionary<string, IConfigMapper>();
+        }
+
+        public object GetResultObject()
+        {
+            foreach (var child in _children)
+            {
+                var propertyName = child.Key;
+                var childConfigMapper = child.Value;
+
+                var propertyInfo = typeof(TItem).GetPropertyInfo(propertyName);
+                
+                propertyInfo.SetValue(_config, childConfigMapper.GetResultObject());
+            }
+
+            return _config;
+        }
+
+        public void Map(List<string> path, object value)
+        {
+            // path[0] - TItem name
+            // path[1] - nested class name OR property name
+
+            if (path.Count < 2)
+                throw new MemberAccessException($"Error. Try map value ({value}) by path {string.Join("/", path)}. " +
+                                                $"Mapper must have path with count gt 2");
+            
+            var propertyName = path[1];
+            var propertyInfo = typeof(TItem).GetPropertyInfo(propertyName);
+            
+            if (path.Count == 2)
+            {
+                propertyInfo.SetValue(_config, value);
+                return;
+            }
+
+            var propetyType = propertyInfo.PropertyType;
+            if (!propetyType.IsClass)
+            {
+                throw new MemberAccessException($"Error. Try map value of nested element of non class property. " +
+                                                $"PropertyType: {propetyType.Name}. Path: {path}. Value: {value}");
+            }
+            
+            var nestedPath = path.ToList();
+            nestedPath.RemoveAt(0);
+
+            if (_children.ContainsKey(propertyName))
+            {
+                var typeArgs = new[] {propetyType};
+                var configMapperType = typeof(ConfigMapper<>).MakeGenericType(typeArgs);
+
+                var configMapper = (IConfigMapper) Activator.CreateInstance(configMapperType);
+
+                _children.Add(propertyName, configMapper);
+            }
+
+            _children[propertyName].Map(nestedPath, value);
+        }
+    }
+}
---
+++ GH.DD.ConfigRetriever/IConfigMapper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GH.DD.ConfigRetriever
+{
+    public interface IConfigMapper
+    {
+        void Map(List<string> path, object value);
+        object GetResultObject();
+    }
+}
