09abb4d12966c08c80dc4216474857ce18d2f77c
Dmitry Demin <demin.ds@mail.ru[>
2018-08-14 01:51:00 +0300

    Add ReflectionExtensions

---
+++ GH.DD.ConfigRetriever/Helpers/ReflectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace GH.DD.ConfigRetriever.Helpers
+{
+    public static class ReflectionExtensions
+    {
+        public static PropertyInfo GetPropertyInfo<TItem>(this TItem item, string propertyName)
+        {
+            var propertyInfo = typeof(TItem).GetProperty(propertyName);
+            if (propertyInfo==null)
+                throw new NullReferenceException($"Property: {propertyName} not found in {typeof(TItem).Name}");
+
+            return propertyInfo;
+        }
+    }
+}
